1b0526480102179db180f6088a51b8a6a37ddbe9
Yinka Erinle <yerinle@gmail.com>
2013-01-01 17:32:41 +0000

    tidy-up for open jdk 7 installation

--- manifests/precise64.pp
+++ manifests/precise64.pp
@@ -1,5 +1,3 @@
-include apt
-
 group { "puppet":
      ensure => "present",
 }
@@ -11,58 +9,24 @@ file { '/etc/motd':
                  Managed by Puppet.\n"
 }
 
+# refresh package manager libraries
+exec { "apt-get update":
+    command => "apt-get update",
+    cwd => "/var/tmp",
+    path    => ["/usr/bin", "/usr/sbin"]
+}
+
 # ensure vim is present
 package { "vim": 
   ensure => present 
 }
 
-exec { "apt-get update":
-    command => "apt-get update",
-    cwd => "/var/tmp",
-    path    => ["/usr/bin", "/usr/sbin"]
-  }
-  
-  apt::source { "partner":
-    location => "http://archive.canonical.com/ubuntu",
-    release => "${lsbdistcodename}",
-    repos => "partner",
-    include_src => false,
-}
-
-# package { jdk:
-#   ensure => installed,
-#   name => $operatingsystem? { 
-#     centOS =>"java-1.6.0-openjdk-devel",
-#     Ubuntu =>"openjdk-7-jdk",
-#     default=>"jdk",
-#   },
-# }
-
-# source => "puppet://$server/filesserver/sun-java6.preseed",
-
-# file { "/var/cache/debconf/sun-java6.preseed":
-#     source => "puppet:///modules/apt/files/etc/sun-java6.preseed",
-#     ensure => present
-# }
-
-
-# file { "/var/cache/debconf/sun-java6.preseed":
-#     ensure => present
-# }
-
-# package { "sun-java6-jdk":
-#     ensure  => installed,
-#     responsefile => "/var/cache/debconf/sun-java6.preseed",
-#     require => [ Apt::Source["partner"], File["/var/cache/debconf/sun-java6.preseed"] ],
-# }
-
-# package { "openjdk-7-jdk":
-#     ensure  => installed,
-#     responsefile => "/var/cache/debconf/sun-java6.preseed",
-#     require => [ Apt::Source["partner"], File["/var/cache/debconf/sun-java6.preseed"] ],
-# }
-
-package { "openjdk-7-jdk":
-    ensure  => installed,
-    require => [ Apt::Source["partner"]],
+# install open jdk 7
+package { jdk:
+  ensure => installed,
+  name => $operatingsystem? { 
+    centOS =>"java-1.7.0-openjdk-devel",
+    Ubuntu =>"openjdk-7-jdk",
+    default=>"jdk",
+  },
 }
